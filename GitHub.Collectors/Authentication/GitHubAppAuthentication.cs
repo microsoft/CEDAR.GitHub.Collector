0052402eb8e7e3f26d3d23e4574dbd0d39140be0
Luke Gostling <37627478+lgostling@users.noreply.github.com>
2022-05-12 11:53:08 -0500

    Add OpenTelemetry to GitHub Collectors (#125)
    * update project file
    
    * add tracing to github functions
    
    * submodule update
    
    * update core submodule
    
    * remove unused import
    
    * Update GitHub.Collectors/Telemetry/GitHubApplicationInsightsTelemetryClient.cs
    
    Co-authored-by: Maximilian Grothusmann <mgrothusmann@microsoft.com>
    
    * sort usings
    
    * moved Activity start() calls to proper locations
    
    * moved Activity construction into async methods to avoid immediate disposal
    
    * remove leftover activity
    
    * mask guid identity substring as valid guid
    
    * Update GitHub.Collectors.Functions/GitHubFunctions.cs
    
    Co-authored-by: Maximilian Grothusmann <mgrothusmann@microsoft.com>
    
    * Update GitHub.Collectors/Authentication/GitHubAppAuthentication.cs
    
    Co-authored-by: Maximilian Grothusmann <mgrothusmann@microsoft.com>
    
    Co-authored-by: Maximilian Grothusmann <mgrothusmann@microsoft.com>

--- GitHub.Collectors/Authentication/GitHubAppAuthentication.cs
+++ GitHub.Collectors/Authentication/GitHubAppAuthentication.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using Azure.Identity;
-using Microsoft.Cloud.InstrumentationFramework;
 using Microsoft.CloudMine.Core.Auditing;
 using Microsoft.CloudMine.Core.Collectors.Authentication;
 using Microsoft.CloudMine.Core.Collectors.Error;
@@ -10,6 +9,7 @@ using Microsoft.CloudMine.Core.Collectors.Web;
 using Microsoft.CloudMine.Core.Telemetry;
 using Microsoft.CloudMine.GitHub.Collectors.Web;
 using Newtonsoft.Json.Linq;
+using OpenTelemetry.Audit.Geneva;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -130,16 +130,20 @@ namespace Microsoft.CloudMine.GitHub.Collectors.Authentication
 
         private void LogTokenGenerationEvent()
         {
-            string callerIdentity = this.Identity.Substring(0, 4); // For security reasons, include only the first 4 characters.
-            TargetResource[] targetResources = new TargetResource[]
+            // For security reasons, include only the first 4 characters.
+            string callerIdentity = this.Identity.Substring(0, 4) + "0000-0000-0000-0000-000000000000";
+
+            List<TargetResource> targetResources = new List<TargetResource>()
             {
                 new TargetResource("Organization", this.organization),
             };
-            CallerIdentity[] callerIdentities = new CallerIdentity[]
+
+            List<CallerIdentity> callerIdentities = new List<CallerIdentity>()
             {
                 new CallerIdentity(CallerIdentityType.ApplicationID, callerIdentity),
             };
-            this.auditLogger.LogTokenGenerationAuditEvent(telemetryClient, OperationResult.Success, targetResources, callerIdentities, TokenType);
+
+            this.auditLogger.LogTokenGenerationAuditEvent(telemetryClient, OperationResult.Success, "",  targetResources, callerIdentities, TokenType);
         }
 
         /// <summary>
